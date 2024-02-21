77cb463133050971ce1c5a36432181511c1efaa5
Jhonathan Abreu <jdabreu25@gmail.com>
2024-02-21 12:25:28 -0400

    Use globals instead of configuration for subscription validation

--- QuantConnect.BitfinexBrokerage/BitfinexBrokerage.cs
+++ QuantConnect.BitfinexBrokerage/BitfinexBrokerage.cs
@@ -549,10 +549,10 @@ namespace QuantConnect.Brokerages.Bitfinex
         {
             try
             {
-                var productId = 182;
-                var userId = Config.GetInt("job-user-id");
-                var token = Config.Get("api-access-token");
-                var organizationId = Config.Get("job-organization-id", null);
+                const int productId = 182;
+                var userId = Globals.UserId;
+                var token = Globals.UserToken;
+                var organizationId = Globals.OrganizationID;
                 // Verify we can authenticate with this user and token
                 var api = new ApiConnection(userId, token);
                 if (!api.Connected)
